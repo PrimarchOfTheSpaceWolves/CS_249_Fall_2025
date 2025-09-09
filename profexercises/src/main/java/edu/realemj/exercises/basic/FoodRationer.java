2367fb9c03a4fd7128f29208f1f965d5b0e85bff
Dr. Michael J. Reale <realemj@sunypoly.edu>
2025-09-09 12:03:20 -0400

    Did some assorted exercises

--- profexercises/src/main/java/edu/realemj/exercises/basic/FoodRationer.java
+++ profexercises/src/main/java/edu/realemj/exercises/basic/FoodRationer.java
@@ -1,21 +1,37 @@
 package edu.realemj.exercises.basic;
 
+import java.util.Scanner;
+
 public class FoodRationer {
     public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+
         // Get total food,
         // total number of people,
         // and rations
-        double totalFood = 200;
-        int numPeople = 5;
-        double rations = 2.5;
+        System.out.println("Enter total food:");
+        double totalFood = input.nextDouble();
+        System.out.println("Enter number of people:");
+        int numPeople = input.nextInt();
+        //double rations = 2.5;
+        final double RATIONS = 2.5;
 
         int x = (int)1.0;
 
         // Calculate food per day
-        double foodPerDay = numPeople * rations;
+        double foodPerDay = numPeople * RATIONS;
 
         // Calculate how many days
+        double daysLeft = totalFood / foodPerDay;
 
         // Print answer
+        System.out.println("Days left: " + daysLeft);
+
+        int ic = 65;
+        char c = (char)ic;
+        System.out.println("Also character is..." + c);
+
+        String s = "" + 9;
+
     }
 }
